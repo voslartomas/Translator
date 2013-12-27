70f7f17f13d74193bf7fd07653dfb3208c8b4846
Tomas Voslar <t.voslar@gmail.com>
2013-12-27 12:17:00 +0000

    updated directories struture, added documentation

---
+++ doc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","BadFunctionCallException"],["c","BadMethodCallException"],["c","Exception"],["c","LogicException"],["c","Webcook\\Translator\\ITranslator"],["c","Webcook\\Translator\\Method"],["c","Webcook\\Translator\\MethodParameter"],["c","Webcook\\Translator\\Results\\LanguageResult"],["c","Webcook\\Translator\\Results\\TranslateResult"],["c","Webcook\\Translator\\ServiceFactory"],["c","Webcook\\Translator\\Services\\YandexTranslator"],["c","Webcook\\Translator\\Translator"]];
