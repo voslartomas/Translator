4ccb6480a31002479ccfccdd06581af7f088a2ac
Tomáš Voslař <tomas.voslar@webcook.cz>
2013-12-27 09:44:01 +0000

    Added doc

--- Results/LanguageResult.php
+++ Results/LanguageResult.php
@@ -12,23 +12,32 @@ class LanguageResult {
 	
 	private $name;
 	
+	/**
+	 * Constructor
+	 * @param String $abbreviation
+	 * @param String $name
+	 */
 	function __construct($abbreviation, $name) {
 		$this->abbreviation = $abbreviation;
 		$this->name = $name;
 	}
-
+	
+	/* Getter for abbreviation. */
 	public function getAbbreviation() {
 		return $this->abbreviation;
 	}
-
+	
+	/* Getter for name. */
 	public function getName() {
 		return $this->name;
 	}
 
+	/* Setter for abbreviation. */
 	public function setAbbreviation($abbreviation) {
 		$this->abbreviation = $abbreviation;
 	}
 
+	/* Setter for name. */
 	public function setName($name) {
 		$this->name = $name;
 	}
