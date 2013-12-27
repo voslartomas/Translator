ae37a209f4f35d1682edfa12196dd9319636f2af
Tomáš Voslař <tomas.voslar@webcook.cz>
2013-12-27 09:42:35 +0000

    Update TranslateResult.php

--- Results/TranslateResult.php
+++ Results/TranslateResult.php
@@ -10,14 +10,20 @@ class TranslateResult {
 	
 	private $translation;
 	
+	/**
+	 * Constructor
+	 * @param String $translation
+	 */
 	public function __construct($translation){
 		$this->translation = $translation;
 	}
 	
+	/* Getter for translation. */
 	public function getTranslation() {
 		return $this->translation;
 	}
 
+	/* Setter for translation. */
 	public function setTranslation($translation) {
 		$this->translation = $translation;
 	}	
