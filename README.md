664032163f223246e6862a4af5ece2b110c0f6c5
Tomáš Voslař <t.voslar@gmail.com>
2013-12-28 18:10:11 +0100

    new settings in factory

--- README.md
+++ README.md
@@ -39,6 +39,30 @@ $languages = $service->getLanguages();
 $translation = $service->translate('Hi, how are you?', 'en', 'cs');
 ```
 
+Or you can use multiple settings in service factory.
+
+```
+$serviceFactory = new Webcook\Translator\ServiceFactory();
+
+$serviceFactory->setSettings(array(
+		\Webcook\Translator\ServiceFactory::YANDEX => array(
+		    'key' => 'Yandex API key'
+		),
+		\Webcook\Translator\ServiceFactory::GOOGLE => array(
+		    'key' => 'Google API key'
+		),
+		\Webcook\Translator\ServiceFactory::BING => array(
+		    'clientId' => 'Bing client id',
+		    'clientSecret' => 'Bing client secret'
+		)
+	    ));
+
+$serviceYandex = $this->serviceFactory->build(\Webcook\Translator\ServiceFactory::YANDEX);
+$serviceGoogle = $this->serviceFactory->build(\Webcook\Translator\ServiceFactory::GOOGLE);
+$serviceBing = $this->serviceFactory->build(\Webcook\Translator\ServiceFactory::BING);
+            
+```
+
 LICENSE
 --
 
--- src/ServiceFactory.php
+++ src/ServiceFactory.php
@@ -17,35 +17,57 @@ class ServiceFactory {
     /* @var Bing service constant. */
     const BING = 3;
     
+    /* @var Array settings for factory */
+    private $settings;
+    
+    public function __construct($settings = array(self::YANDEX => array(), self::GOOGLE => array(), self::BING => array())) {
+	$this->settings = $settings;
+    }
+    
     /**
      * Creates service.
      * @return Translator
      */
     public function build($service, $params = array()){
-
+	
 	switch ($service) {
 	    case self::YANDEX:
 
-		if(!array_key_exists('key', $params)){
+		if(!array_key_exists('key', $params) && !array_key_exists('key', $this->settings[self::YANDEX])){
 		    throw new \BadMethodCallException('You must provide `key` (Yandex API key) parameter.');
+		}else{
+		    if(!array_key_exists('key', $params)){
+			$params['key'] = $this->settings[self::YANDEX]['key'];
+		    }
 		}
 
 		return new Services\YandexTranslator($params['key']);
 		
 	    case self::GOOGLE:
 
-		if(!array_key_exists('key', $params)){
+		
+		if(!array_key_exists('key', $params) && !array_key_exists('key', $this->settings[self::GOOGLE])){
 		    throw new \BadMethodCallException('You must provide `key` (Google Translate API key) parameter.');
+		}else{
+		    if(!array_key_exists('key', $params)){
+			$params['key'] = $this->settings[self::GOOGLE]['key'];
+		    }
 		}
 
 		return new Services\GoogleTranslator($params['key']);
 		
 	    case self::BING:
 
-		if(!array_key_exists('clientId', $params) ||!array_key_exists('clientSecret', $params)){
-		    throw new \BadMethodCallException('You must provide client ID and client secret for your application.');
+		if((!array_key_exists('clientId', $params) || !array_key_exists('clientSecret', $params)) &&
+		    (!array_key_exists('clientId', $this->settings[self::BING]) || !array_key_exists('clientSecret', $this->settings[self::BING]))){
+			throw new \BadMethodCallException('You must provide client ID and client secret for your application.');
+		}else{
+		    if(!array_key_exists('clientId', $params)){
+			$params['clientId'] = $this->settings[self::BING]['clientId'];
+			$params['clientSecret'] = $this->settings[self::BING]['clientSecret'];
+		    }
 		}
-
+		
 		return new Services\BingTranslator($params['clientId'], $params['clientSecret']);
 		
 	    default:
@@ -59,5 +81,13 @@ class ServiceFactory {
 	    self::GOOGLE => 'Google',
 	    self::BING => 'Bing'
 	);
+    }
+    
+    public function getSettings() {
+	return $this->settings;
+    }
+
+    public function setSettings($settings) {
+	$this->settings = $settings;
     }
 }
